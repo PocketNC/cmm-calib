77746321a34dc85a32e40896ff8a04e47eb23191
Jake Danczyk <jake@pocketnc.com>
2023-02-09 11:10:55 -0700

    Set basic stage order SOFT-1030

--- generateStageLists.py
+++ generateStageLists.py
@@ -6,11 +6,12 @@ POCKETNC_VAR_DIR = os.environ.get('POCKETNC_VAR_DIRECTORY')
 CALIB_DIR = os.path.join(POCKETNC_VAR_DIR, 'calib')
 if not os.path.exists(CALIB_DIR):
   os.makedirs(CALIB_DIR)
-STAGE_LISTS_FILE = os.path.join(CALIB_DIR, 'stage_lists.json')
+# STAGE_LISTS_FILE = os.path.join(CALIB_DIR, 'stage_lists.json')
+STAGE_LISTS_FILE = 'stage_lists.json'
+
 
 basic_calib_order = [
   Stages.ERASE_COMPENSATION,
-  Stages.SETUP_CNC,
   Stages.SETUP_CMM,
   Stages.PROBE_MACHINE_POS,
   Stages.PROBE_SPINDLE_POS,
@@ -21,13 +22,15 @@ basic_calib_order = [
   Stages.PROBE_FIXTURE_BALL_POS,
   Stages.HOMING_Y,
   Stages.CHARACTERIZE_Y,
-  Stages.PROBE_TOP_PLANE,
-  Stages.PROBE_HOME_OFFSETS,
+  Stages.PROBE_OFFSETS,
   Stages.HOMING_A,
   Stages.HOMING_B,
-  Stages.CHARACTERIZE_A_SPHERE,
-  Stages.CHARACTERIZE_B_SPHERE,
-  Stages.TOOL_PROBE_OFFSET,
+  Stages.CHARACTERIZE_A_LINE,
+  Stages.CHARACTERIZE_B_LINE,
+  Stages.CALIBATE,
+  Stages.VERIFY_OFFSETS,
+  Stages.VERIFY_A_LINE,
+  Stages.VERIFY_B_LINE,
 ]
 
 advanced_calib_order = [
